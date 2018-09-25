50144f5c3352edb5b8635da202ad73d35d202d68
cherishPhoebe <cherish1224@outlook.com>
2018-09-26 00:11:37 +0800

    session 01

---
+++ CoreWebApp/Middleware/HttpContextItemsMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApp.Middleware
+{
+    public class HttpContextItemsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public static readonly object HttpContextItemsMiddlewareKey = new object();
+
+        public HttpContextItemsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext) {
+            httpContext.Items[HttpContextItemsMiddlewareKey] = "k-9";
+
+            await _next(httpContext);
+        }
+    }
+
+
+    public static class HttpContextItemsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHttpContextItemsMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HttpContextItemsMiddleware>();
+        }
+    }
+}
